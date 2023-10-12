47562afb37eca130a233d5cc0f2c85ca99a4ebeb
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-10-12 13:21:37 -0600

    chore

--- _my-redis/src/bin/tokio-xtras-002-sync-spawn.rs
+++ _my-redis/src/bin/tokio-xtras-002-sync-spawn.rs
@@ -10,12 +10,19 @@ use tokio::time::{sleep, Duration};
  * - ❌ Run the Runtime in a separate thread and send messages to it.
  */
 fn main() {
+    /*
+     * it is important that the runtime is configured
+     * to be a multi_thread runtime.
+     *
+     * If you change it to be a current_thread runtime,
+     * you will find that the time consuming task finishes
+     * before any of the background tasks start.
+     */
     // let runtime = Builder::new_multi_thread()
     //     .worker_threads(1)
     //     .enable_all()
     //     .build()
     //     .unwrap();
-
     let runtime = tokio::runtime::Builder::new_current_thread()
         /*
          * The enable_all call enables the IO and timer drivers on the Tokio runtime.
@@ -28,6 +35,11 @@ fn main() {
     let mut handles = Vec::with_capacity(10);
 
     for i in 0..10 {
+        /*
+         * The spawn method is also available on the Handle type.
+         * The Handle type can be cloned to get many handles to a runtime,
+         * and each Handle can be used to spawn new tasks on the runtime.
+         */
         handles.push(runtime.spawn(my_big_task(i)));
     }
 
@@ -38,6 +50,23 @@ fn main() {
 
     //? Wait for all of them to complete.
     for handle in handles {
+        /*
+         * As an example, this could be a good way of implementing
+         * background network requests in a graphical application
+         * because network requests are too time consuming to run
+         * them on the main GUI thread.
+         *
+         * waits for the spawned tasks to finish by calling block_on on
+         * the JoinHandle returned by the call to spawn,
+         * but this isn't the only way to do it. Here are some alternatives:
+         *
+         * - Use a message passing channel such as tokio::sync::mpsc.
+         *
+         * - Modify a shared value protected by e.g. a Mutex.
+         *  This can be a good approach for a progress bar in a GUI,
+         * where the GUI reads the shared value every frame.
+         */
+        // todo: Use a message passing channel such as tokio::sync::mpsc.
         //? The `spawn` method returns a `JoinHandle`. A `JoinHandle` is
         //? a future, so we can wait for it using `block_on`.
         runtime.block_on(handle).unwrap();
